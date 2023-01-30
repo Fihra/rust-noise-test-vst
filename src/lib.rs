3bd9f39082e826770b6253c0cca5ed82c0ae4ef1
fihra <fabrofabian@gmail.com>
2023-01-30 00:21:45 -1000

    deleted whisper and added basicPlugin boilerplate for official docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,22 @@
 #[macro_use]
 extern crate vst;
 
-use vst::plugin::{Info, Plugin, Category};
+use vst::plugin::{HostCallback, Info, Plugin};
 
-#[derive(Default)]
-struct Whisper;
+struct BasicPlugin;
 
-impl Plugin for Whisper {
+impl Plugin for BasicPlugin {
+    fn new(_host:HostCallback) -> Self {
+        BasicPlugin
+    }
+
     fn get_info(&self) -> Info {
         Info {
-            name: "Whisper".to_string(),
-            unique_id: 1337,
-            inputs: 0,
-            outputs: 2,
-            category: Category::Synth,
+            name: "Basic Plugin".to_string(),
+            unique_id: 1357,
             ..Default::default()
         }
     }
 }
 
-plugin_main!(Whisper);
+plugin_main!(BasicPlugin);
